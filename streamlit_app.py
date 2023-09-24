a5e0decc2aa4bd74ad908d83f8488982fec47082
İBRAHİM MERT İNAN <145788922+Satyridos@users.noreply.github.com>
2023-09-24 15:57:38 +0300

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import plotly.express as px
 import streamlit as st
 
 # Display title and text
-st.title("Week 1 - Data and Visualization")
-st.markdown("Here we can see the dataframe created during this week's project.")
+st.title("Week 1 - Data and visualization")
+st.markdown("Here we can see the dataframe created during this weeks project.")
 
 # Read dataframe
 dataframe = pd.read_csv(
@@ -26,7 +26,8 @@ dataframe = dataframe[dataframe["Price"] <= 100]
 # Display as integer
 dataframe["Airbnb Listing ID"] = dataframe["Airbnb Listing ID"].astype(int)
 # Round of values
-dataframe["Price"] = "₺ " + dataframe["Price"].round(2).astype(str)
+dataframe["Price"] = "₺ " + dataframe["Price"].round(2).astype(str) # <--- CHANGE THIS POUND SYMBOL IF YOU CHOSE CURRENCY OTHER THAN POUND
+# Rename the number to a string
 dataframe["Location"] = dataframe["Location"].replace(
     {1.0: "To visit", 0.0: "Airbnb listing"}
 )
